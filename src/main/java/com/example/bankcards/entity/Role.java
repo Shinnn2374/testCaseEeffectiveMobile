3c73b07121a131d237a87f3ae5046b2f03329f7d
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2025-08-05 12:00:33 +0500

    ...

---
+++ src/main/java/com/example/bankcards/entity/Role.java
@@ -0,0 +1,12 @@
+package com.example.bankcards.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Role {
+    @Id
+    private String name;
+}
